a6ad8608d0ee573a0dbc89600e9a3688b7a75c64
Adam B <a.bernas@interia.pl>
2022-03-13 14:48:00 +0100

    Update 4. Raport- lista warunków szkodliwych pracowników.sql

--- 4. Raport- lista warunków szkodliwych pracowników.sql
+++ 4. Raport- lista warunków szkodliwych pracowników.sql
@@ -1,28 +1,28 @@
---Tytu³: Raport z list¹ warunków szkodliwych pracowników
---Autor: Adam Bernaœ
+ï»¿--TytuÅ‚: Raport z listÄ… warunkÃ³w szkodliwych pracownikÃ³w
+--Autor: Adam BernaÅ›
 --Update: 10-03-2022
 --Version v1.1
 
-/*Podgl¹d raportu
+/*PodglÄ…d raportu
 SELECT * FROM dbo.View_EmpHarmCond
 */
 USE EmployeeMedicalTest
 GO
 
---Usuñ widok je¿eli istnieje
+--UsuÅ„ widok jeÅ¼eli istnieje
 IF OBJECT_ID ('dbo.View_EmpHarmCond') IS NOT NULL DROP VIEW dbo.View_EmpHarmCond
 GO
 
---Tworzenie widoku z opcj¹ SCHEMABINDING oraz CHECK OPTION
+--Tworzenie widoku z opcjÄ… SCHEMABINDING oraz CHECK OPTION
 
 CREATE VIEW dbo.View_EmpHarmCond
 WITH SCHEMABINDING
 AS
 
 SELECT   
-		E.Name											  as [Imiê i nazwisko],
+		E.Name											  as [ImiÄ™ i nazwisko],
 		HC.Name											  as [Warunki szkodliwe],
-		COUNT(*)OVER(PARTITION BY E.Name ORDER BY E.Name) as [Suma warunków szkodliwych]
+		COUNT(*)OVER(PARTITION BY E.Name ORDER BY E.Name) as [Suma warunkÃ³w szkodliwych]
 FROM dbo.Employee				as E
 JOIN dbo.EmployeeHarmCond		as EHC
 	ON E.IdEmp = EHC.IdEmp
